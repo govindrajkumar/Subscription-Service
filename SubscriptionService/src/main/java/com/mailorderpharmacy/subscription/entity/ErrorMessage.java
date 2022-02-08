46cb393e6f1036932b69d2ab36c1325f3f56f05e
govindrajkumar <govindrajkumarg123@gmail.com>
2022-02-08 16:56:22 +0530

    Subscription-commit

---
+++ SubscriptionService/src/main/java/com/mailorderpharmacy/subscription/entity/ErrorMessage.java
@@ -0,0 +1,26 @@
+package com.mailorderpharmacy.subscription.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorMessage {
+	// http status
+	private HttpStatus status;
+
+	// timestamp
+	private LocalDateTime timestamp;
+
+	// message
+	private String message;
+
+}
---
+++ SubscriptionService/src/main/java/com/mailorderpharmacy/subscription/entity/TokenValid.java
@@ -0,0 +1,25 @@
+package com.mailorderpharmacy.subscription.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenValid {
+
+	// User Id
+	private String uid;
+
+	// User Name
+	private String name;
+
+	// Validity check for the token
+	private boolean isValid;
+
+}
